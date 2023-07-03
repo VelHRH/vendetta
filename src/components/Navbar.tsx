56e82f18fd6129960f19b2668b55eb2bcdf2c177
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-07-03 15:21:10 +0300

    logo 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = async () => {
   <div className="fixed h-[80px] text-xl z-50 bg-slate-100/95 dark:bg-slate-900/95 backdrop-blur-sm flex justify-between items-center top-0 left-[50%] translate-x-[-50%] px-10 w-full">
    <div className="flex gap-14 items-center">
     <Link href="/" className="flex gap-2 font-bold items-center">
-     <Icons.logo className="h-[50px] w-[52px] text-slate-900 dark:text-slate-100" />
+     <Icons.logo className="h-[45px] w-[47px] text-slate-900 dark:text-slate-100" />
      <p>Vendetta</p>
     </Link>
     <div className="flex gap-5 items-center">
