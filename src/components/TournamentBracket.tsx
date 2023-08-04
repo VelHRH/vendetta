9381e0a2a9a3d64e04a7481c95d98806ca4bbf3c
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-08-04 21:36:28 +0300

    trying to assign width

--- src/components/TournamentBracket.tsx
+++ src/components/TournamentBracket.tsx
@@ -76,7 +76,7 @@ const TournamentBracket: FC<TournamentBracketProps> = ({
            orderedMatches[ind].map((elem, i) => (
             <div
              key={i}
-             className="p-1 dark:bg-slate-700 bg-slate-300 h-1/2 rounded-t-md flex"
+             className="p-1 dark:bg-slate-700 bg-slate-300 h-1/2 rounded-t-md flex w-auto flex-wrap overflow-hidden"
             >
              <MatchSide wrestlers={orderedMatches[ind][i]} />
             </div>
