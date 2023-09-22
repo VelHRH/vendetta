4fee6035976d917a73937a883828b8ff103c248d
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-09-22 23:00:28 +0300

    october update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default async function Home() {
           : nextShow.name}{" "}
         </Label>
         <InfoElement>
-         <p className="text-lg lg:text-2xl font-medium">August 2023</p>
+         <p className="text-lg lg:text-2xl font-medium">October 2023</p>
         </InfoElement>
        </div>
        <div className="w-full flex-1 p-3 lg:p-5 border-2 border-slate-300 dark:border-slate-700 rounded-md">
